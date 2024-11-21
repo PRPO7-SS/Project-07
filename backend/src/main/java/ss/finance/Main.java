5dcda625914a07183f50f3960c529c1990c6390b
svitj123 <sj83713@student.uni-lj.si>
2024-11-21 21:41:54 +0100

    Dodan KumuluzEE

---
+++ backend/src/main/java/ss/finance/Main.java
@@ -0,0 +1,10 @@
+package ss.finance;
+
+import com.kumuluz.ee.EeApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Application started!");
+        EeApplication.main(args);
+    }
+}
---
+++ backend/src/main/java/ss/finance/RestApplication.java
@@ -0,0 +1,9 @@
+package ss.finance;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+    // No additional configuration needed for now.
+}
